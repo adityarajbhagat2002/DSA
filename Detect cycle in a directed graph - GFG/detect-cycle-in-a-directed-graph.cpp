78f3ca6b3e0451a17a2158e698c5939433ce4da6
Aditya Raj Bhagat <90248806+adityarajbhagat2002@users.noreply.github.com>
2022-11-09 01:24:04 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+    public:
+    bool dfs(int node ,int vis[] ,int pathvis[], vector<int> adj[]){
+        vis[node]=1;
+        pathvis[node]=1;
+        
+        for(auto it : adj[node]){
+            //if the node is not visited
+            if(!vis[it]){
+                if(dfs(it,vis,pathvis,adj)==true)
+                return true;
+            }
+            //if the node is previously visited
+            //but not on the same path
+            else if(pathvis[it])
+            return true;
+        }
+        pathvis[node]=0;
+        return false;
+    }
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        int vis[V]={0};
+        int pathvis[V]={0};
+        
+        for(int i=0 ; i<V; i++){
+            if(!vis[i]){
+                if(dfs(i, vis, pathvis, adj)==true)
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        cout << obj.isCyclic(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
