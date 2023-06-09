c05b0ab2cbfba1460e0709865503592e4427b95f
Aditya Raj Bhagat <90248806+adityarajbhagat2002@users.noreply.github.com>
2023-06-09 13:37:12 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge Sort/merge-sort.cpp
+++ Medium/Merge Sort/merge-sort.cpp
@@ -21,39 +21,76 @@ class Solution
     public:
     void merge(int arr[], int l, int m, int r)
     {
-int temp[r-l+1];
+         int temp[r-l+1];
          int left=l;
          int right=m+1;
          int x=0;
-         while(left<=m && right<=r)
-         {
-             if(arr[left]<=arr[right])
-             {
+         
+         while(left <= m && right <=r){
+             if(arr[left]<=arr[right]){
                  temp[x++]=arr[left];
                  left++;
              }
-             else
-             {
+             else{
                  temp[x++]=arr[right];
                  right++;
+                 
              }
              
          }
-             while(left<=m)
-            { temp[x++]=arr[left];
-             left++;}
-             while(right<=r)
-            { temp[x++]=arr[right];
-             right++;}
+         while(left<=m)
+         {
+             temp[x++]=arr[left];
+             left++;
              
-             for (int i = l; i <= r; i++) {
-        arr[i] = temp[i - l];
-    }
+         }
+         while(right<=r)
+        {
+             temp[x++]=arr[right];
+             right++;
+        }
         
+        for(int i=l;i<=r; i++){
+            arr[i]=temp[i-l];
+        }
+        
+        
+        
+        
+    //     int temp[r-l+1];
+    //      int left=l;
+    //      int right=m+1;
+    //      int x=0;
+    //      while(left<=m && right<=r)
+    //      {
+    //          if(arr[left]<=arr[right])
+    //          {
+    //              temp[x++]=arr[left];
+    //              left++;
+    //          }
+    //          else
+    //          {
+    //              temp[x++]=arr[right];
+    //              right++;
+    //          }
+             
+    //      }
+    //          while(left<=m)
+    //         { temp[x++]=arr[left];
+    //          left++;}
+    //          while(right<=r)
+    //         { temp[x++]=arr[right];
+    //          right++;}
+             
+    //          for (int i = l; i <= r; i++) {
+    //     arr[i] = temp[i - l];
+    // }
+         
     }
     public:
     void mergeSort(int arr[], int l, int r)
-    {   if(l>=r)return;
+    {
+        if(l>=r)return;
         int mid=(l+r)/2;
         mergeSort(arr,l,mid);
         mergeSort(arr,mid+1,r);
