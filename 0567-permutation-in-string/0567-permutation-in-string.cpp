26acbcd170aa6408d99ee172a487f4b0635571ca
Aditya Raj Bhagat <90248806+adityarajbhagat2002@users.noreply.github.com>
2023-09-21 12:19:22 +0530

    Time: 3 ms (95.61%), Space: 7.5 MB (32.91%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,18 +1,57 @@
 class Solution {
+    private : 
+    bool checkEqual(int a[26] ,int  b[26]){
+        for(int i=0; i<26; i++){
+            if(a[i]!=b[i])return 0;
+        }
+        return 1;
+    }
+    
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.length()<s1.length())return false;
-        if(s1.length()<=1 && s2.length()<=1){
-            if(s1==s2)return true;
-            else return false;
+        //Counting the array
+        
+        int count1[26]={0};
+        for(int i=0; i<s1.length(); i++){
+            int index = s1[i] -'a';
+            count1[index]++;
         }
-        sort(s1.begin(),s1.end());
-        for(int i=0;i<=s2.length()-s1.length();i++){
-            string s=s2.substr(i,s1.length());
-            sort(s.begin(),s.end());
-            if(s==s1)return true;
+        
+        //we traverse through the S2 string in a window size of S1 and compare
+        int i=0;
+        int windowSize=s1.length();
+        int count2[26]={0};
+        
+        //running for the first window
+        
+        while( i < windowSize && i<s2.length()){
+            int index = s2[i]-'a';
+            count2[index]++;
+            i++;            
         }
-        return false;
+        
+        if(checkEqual(count1, count2))return 1;
+        
+        //next window
+        while(i<s2.length()){
+            char newChar = s2[i];
+            int index = newChar-'a';
+            count2[index]++;
+            
+            
+            char oldChar=s2[i-windowSize];
+            index =oldChar -'a';
+            count2[index]--;
+            i++;
+            
+            
+             if(checkEqual(count1, count2))return 1;
+            
+        }
+        return 0;
+        
+        
+        
         
     }
 };
