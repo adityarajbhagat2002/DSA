f7446aafbb09293afd9b8b7c7543234ea3522de6
Aditya Raj Bhagat <90248806+adityarajbhagat2002@users.noreply.github.com>
2023-09-21 11:32:32 +0530

    Time: 0 ms (100.00%), Space: 7 MB (18.58%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -4,9 +4,7 @@ public:
         
        while(s.length()!=0 && s.find(part)< s.length()){
            s.erase(s.find(part) , part.length());
-       }
-        
+       }      
         return s;
-        
     }
 };
